91ffad3a8d34f93093e3081c77be99b9123ccef2
umiwatari823 <umiwatari823@gmail.com>
2022-02-13 00:28:14 +0900

    !!の追加

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.こんにちは世界")
+  return HttpResponse("Hello, world. You're at the polls index.こんにちは世界!!")
 
 # Create your views here.
