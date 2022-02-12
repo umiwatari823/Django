43f9e49887d03a2e22d7bc858a2c213fac099e32
umiwatari823 <umiwatari823@gmail.com>
2022-02-13 00:05:55 +0900

    こんにちは世界の追
    練習のため加

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.こんにちは")
+  return HttpResponse("Hello, world. You're at the polls index.こんにちは世界")
 
 # Create your views here.
